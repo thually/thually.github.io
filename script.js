910115c5e14271123b9d228c7d0fbb28944ba77f
Alejandro Salazar Mejía <alejosalazarmejia.2000@gmail.com>
2023-09-02 12:36:19 -0500

    Added the recomendation functionality

--- script.js
+++ script.js
@@ -11,10 +11,17 @@ function addRecommendation() {
       element.setAttribute("class","recommendation");
       element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
       // Add this element to the end of the list of recommendations
+
+      let author = document.getElementById("author");
+      if (author.value != null && author.value.trim() != "") {
+        element.innerHTML += "\<span class=\"author\"\> ~ " + author.value + "\</span\>";
+      }
+
       document.getElementById("all_recommendations").appendChild(element); 
       
       // Reset the value of the textarea
       recommendation.value = "";
+      author.value = "";
     }
   }
   
